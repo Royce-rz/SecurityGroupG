d72573cc67c5dd3126b7ef2fdfd4328a5001e3e4
ChristopherQC <114951326+ChristopherQC@users.noreply.github.com>
2024-04-16 23:22:18 +0100

    Add files via upload

--- web/my_app.py
+++ web/my_app.py
@@ -1,9 +1,26 @@
-from flask import Flask, render_template, redirect, flash, request, session
+from flask import Flask, render_template, redirect, flash, request, session, send_file, make_response
+from flask_mail import Mail, Message
 import pymssql
-
+import hashlib
+import random, string, base64, os
+from captcha.image import ImageCaptcha
+    
 app = Flask(__name__)
-app.secret_key = 'your_secret_key' #cookies
 
+app.secret_key = 'QiChen_specialKey' #cookies
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = 'qipublic11@gmail.com'
+app.config['MAIL_PASSWORD'] = 'druykvpazezleseu'
+app.config['MAIL_DEFAULT_SENDER'] = ('QiChen', 'qipublic11@gmail.com')
+
+mail = Mail(app)
+
+
+file_abs_path = os.path.abspath(__file__)
+file_path = os.path.dirname(file_abs_path)
 #connect to sql database
 db = pymssql.connect(
     server = 'qiscerver.database.windows.net',
@@ -19,34 +36,29 @@ cursor = db.cursor()
 
 
 #command to database
-def createUser(username, password):
-    try:
-        sql = "insert into users(username,password) values('"+username+"','"+password+"')"
-        #sql = 'update students set name=%s where id=%s'
-        #sql = 'delete from students where id<%s'
-        cursor.execute(sql)
-        db.commit()
-    except:
-        db.rollback()
+def createUser(username,password,email,verifyCode,illness):
+    sql = "insert into users(Username,Password,Email,VerifyCode,Illness) values('"+username+"','"+password+"','"+email+"','"+verifyCode+"','"+illness+"')"
+    #sql = 'update students set name=%s where id=%s'
+    #sql = 'delete from students where id<%s'
+    cursor.execute(sql)
+    db.commit()
 
-def detectUser(username):
-    sql = 'select * from users where username=%s'
-    cursor.execute(sql,(username))
+def deleteUser(key, value):
+    sql = 'delete from users where ' + str(key) + '=%s'
+    cursor.execute(sql,(value))
+    db.commit()
+
+def detectUser(key, value):
+    sql = 'select * from users where ' + str(key) + '=%s'
+    cursor.execute(sql,(value))
     detected_user = cursor.fetchall()
-    if len(detected_user) > 0:
-        return 1
-    else:
-        return 0
-
+    return detected_user
 
 def loginUser(username, password):
     sql = 'select * from users where username=%s and password=%s'
     cursor.execute(sql,(username,password))
     exist_user = cursor.fetchall()
-    if len(exist_user) > 0:
-        return 1
-    else:
-        return 0
+    return exist_user
 
 def user_logged_in():
     if 'username' in session:
@@ -54,6 +66,30 @@ def user_logged_in():
     else:
         return 0
 
+def encode_password(password):
+    ePw=hashlib.md5()
+    ePw.update('try to encode this'.encode('utf-8'))
+    ePw.update(password.encode('utf-8'))
+    return str(ePw.hexdigest())
+
+def generate_Code(length):
+    characters = string.ascii_letters + string.digits
+    Code = ''.join(random.choice(characters) for i in range(length))
+    return Code
+
+def generate_captcha():  
+    captcha = generate_Code(4)
+    #creating captcha img
+    img = ImageCaptcha(width=200)
+    img.generate(captcha)
+    img.write(captcha, 'static/images/captcha.png')
+    return captcha
+
+def send_code(email):
+    session['code'] = generate_Code(6)
+    message = Message(subject="Verify Code", recipients=[email])
+    message.body = "Your verify code is " + session['code']
+    mail.send(message)
 
 
 #website
@@ -70,26 +106,41 @@ def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        if loginUser(username, password):
-            session['username'] = username
-            flash('successfully login!')
-            return redirect('/')
+        captcha = request.form['captcha']
+        password = encode_password(password)
+        if session['captcha'].lower() == captcha.lower():
+            if len(loginUser(username, password)) > 0:
+                session['username'] = username
+                flash('successfully login!')
+                return redirect('/')
+            else:
+                flash('Username or password incorrect!')
         else:
-            flash('Falure')
+            flash('captcha incorrect!')
     return render_template('login.html', user_logged_in=user_logged_in())
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        #get form
         username = request.form['username']
+        email = request.form['email']
         password = request.form['password']
- 
-        if detectUser(username):
+        illness = request.form['ill']
+        verifyCode = request.form['verify_code']
+        #encode password
+        password = encode_password(password)
+        #check
+        if len(detectUser('username', username)) > 0:
             flash('Username existing')
-        else:
-            createUser(username, password)
+        elif verifyCode == "":
+            send_code(email)
+        elif session['code'] == verifyCode:
+            createUser(username,password,email,verifyCode, illness)
             flash('Successfully registered!')
             return redirect('/login')
+        else:
+            flash('Wrong verify code')
     return render_template('register.html', user_logged_in=user_logged_in())
 
 @app.route('/logout')
@@ -120,7 +171,7 @@ def user_id(user_id):
 @app.post("/search")
 def search():
     user_name = request.form['user_name']
-    if not detectUser(user_name):
+    if len(detectUser('username', user_name)) == 0:
         flash("Username is not existed")
         return redirect('/data')
     else:
@@ -128,6 +179,12 @@ def search():
         cursor.execute(sql, (user_name))
         users = cursor.fetchall()
         return redirect('/data/' + str(users[0]['id']))
+    
+@app.get("/captcha")
+def captcha():
+    captcha = generate_captcha()
+    session['captcha'] = captcha
+    return send_file(file_path + '/static/images/captcha.png', mimetype='image/png')
 
 cursor.close
 
