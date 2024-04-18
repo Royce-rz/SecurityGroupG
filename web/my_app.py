588ddb507216dafe5bdc4a2cba1081c83256b6c8
ChristopherQC <114951326+ChristopherQC@users.noreply.github.com>
2024-04-18 18:25:34 +0100

    Update my_app.py

--- web/my_app.py
+++ web/my_app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, flash, request, session, sen
 from flask_mail import Mail, Message
 import pymssql
 import hashlib
-import random, string, base64, os
+import random, string, base64, os, re
 from captcha.image import ImageCaptcha
 import socket
     
@@ -92,11 +92,22 @@ def generate_captcha():
     return captcha
 
 def send_code(email):
-    session['code'] = generate_Code(6)
-    message = Message(subject="Verify Code", recipients=[email])
-    message.body = "Your verify code is " + session['code']
-    mail.send(message)
+    try:
+        session['code'] = generate_Code(6)
+        message = Message(subject="Verify Code", recipients=[email])
+        message.body = "Your verify code is " + session['code']
+        mail.send(message)
+    except:
+        flash("Unvalid email address!")
 
+def parametrize_string(string):
+    pattern = re.compile(r"[;\-']")
+    match = pattern.search(string)
+    print(match)
+    if match == None:
+        return 1
+    else:
+        return 0
 
 #website
 @app.route('/')
@@ -115,8 +126,6 @@ def login():
         captcha = request.form['captcha']
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
-        # ip_address = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)  # Capture IP address
-
         password = encode_password(password)
         if session.get('captcha', '').lower() == captcha.lower():
             user = loginUser(username, password)
@@ -129,6 +138,8 @@ def login():
                 flash('Username or password incorrect!')
         else:
             flash('Captcha incorrect!')
+        
+        
 
     return render_template('login.html', user_logged_in=user_logged_in())
 
@@ -177,22 +188,25 @@ def dataPreform():
 def user_id(user_id):
     if not user_logged_in():
         return render_template('return.html', info="You need to login first!")
-    sql = 'SELECT * FROM users where id = %s'
-    cursor.execute(sql, (user_id))
+    sql = 'SELECT * FROM users where id =' +user_id
+    cursor.execute(sql)
     users = cursor.fetchall()
     return render_template('dataPreform.html', users=users, user_logged_in=user_logged_in())
 
 @app.post("/search")
 def search():
     user_name = request.form['user_name']
-    if len(detectUser('username', user_name)) == 0:
-        flash("Username is not existed")
-        return redirect('/data')
+    if parametrize_string(user_name):
+        if 0: #len(detectUser('username', user_name)) == 0:
+            flash("Username is not existed")
+            return redirect('/data')
+        else:
+            sql = "select * from users where username=%s"
+            cursor.execute(sql,(user_name))
+            users = cursor.fetchall()
+            return redirect('/data/' + str(users[0]['Id']))
     else:
-        sql = 'select * from users where username=' + user_name
-        cursor.execute(sql)
-        users = cursor.fetchall()
-        return redirect('/data/' + str(users[0]['Id']))
+        flash("Please enter valid character!!!")
     
 @app.get("/captcha")
 def captcha():
